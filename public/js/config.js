9ff0ada0adb1e049b40252a3a3c981354d0ffc4b
Andriy Nakonechnyy <andriy.nakonechnyy@vakoms.com.ua>
2017-02-03 16:08:41 +0000

    Added config.js

---
+++ public/js/config.js
@@ -0,0 +1,28 @@
+// development
+var chromeRuntimeKey = 'ooilnppgcbcdgmomhgnbjjkbcpfemlnj';
+var biomIoPort = ':5002'; // 5001 -> for production
+var developmentPort = ':5002'; // from prod  developmentPort = ''; is empty
+var displayQrCode = true;
+var basicUrl = 'https://ai-dev.biom.io';
+
+var openIdProviderToken = basicUrl + biomIoPort +'/user/authorize?response_type=code&external_token={external_token}' +
+    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
+    '/login/openId';
+var openIdProvider = basicUrl + biomIoPort +'/user/authorize?response_type=code' +
+    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
+    '/login/openId';
+
+
+var getToken = function(external_token){
+    if(displayQrCode) {
+        window.open(openIdProvider,'null','width=460, height=485, location=no');
+    } else {
+        if (external_token.length != 0) {
+            window.open(
+                openIdProviderToken.replace('{external_token}',external_token),
+                'null','width=460, height=485, location=no'
+            );
+        }
+    }
+    return false;
+}
