95d898365c4addbd5b5c7a1165e73d23bb99e96b
root <andriy.nakonechnyy@vakoms.com>
2017-02-15 10:50:38 +0000

    Configured parameters for development branch and idp-dev

--- public/js/config.js
+++ public/js/config.js
@@ -2,14 +2,14 @@
 var chromeRuntimeKey = 'ooilnppgcbcdgmomhgnbjjkbcpfemlnj';
 var biomIoPort = ':443'; // 5001 -> for production
 var developmentPort = ':443'; // from prod  developmentPort = ''; is empty
-var displayQrCode = true;
+var displayQrCode = false;
 var basicUrl = 'https://idp-dev.biom.io';
 
 var openIdProviderToken = basicUrl + biomIoPort +'/user/authorize?response_type=code&external_token={external_token}' +
-    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://idp-dev.biom.io' + developmentPort +
+    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
     '/login/openId';
 var openIdProvider = basicUrl + biomIoPort +'/user/authorize?response_type=code' +
-    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://idp-dev.biom.io' + developmentPort +
+    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
     '/login/openId';
 
 
