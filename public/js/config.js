60849b07b592dc15e1a0c1e8f752a9a9f765e03f
Dartha Claus <darthaclaus@gmail.com>
2017-05-19 17:52:48 +0300

    Hardcoded providerId and webresourceId.

--- public/js/config.js
+++ public/js/config.js
@@ -6,10 +6,10 @@ var displayQrCode = false;
 var basicUrl = 'https://idp-dev.biom.io';
 
 var openIdProviderToken = basicUrl + biomIoPort +'/user/authorize?response_type=code&external_token={external_token}' +
-    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
+    '&scope=openid&provider_id=55&webresource_id=14&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
     '/login/openId';
 var openIdProvider = basicUrl + biomIoPort +'/user/authorize?response_type=code' +
-    '&scope=openid&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
+    '&scope=openid&provider_id=55&webresource_id=14&client_id=56ce9a6a93c17d2c867c5c293482b8f9&nonce=12p6bfw&state=1slw5l8&redirect_uri=https://ai-dev.biom.io' + developmentPort +
     '/login/openId';
 
 
